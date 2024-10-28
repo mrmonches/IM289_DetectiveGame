f1028add20196098274c9f7217143f2eeac6fc17
Lilum888 <jparrott@mail.bradley.edu>
2024-10-28 12:32:50 -0500

    Typerwiter is not dumb
    It works and doesnt look like stupid code

--- Assets/Scripts/TypeWriterController.cs
+++ Assets/Scripts/TypeWriterController.cs
@@ -19,12 +19,12 @@ public class TypeWriterController : MonoBehaviour
     private int option1;
     private int option2;
     private int option3;
-    private bool correctCheck1=false;
-    private bool correctCheck2=false;
-    private bool correctCheck3=false;
+    private bool correctCheck1 = false;
+    private bool correctCheck2 = false;
+    private bool correctCheck3 = false;
     private bool correctCheck4 = false;
     private bool correctCheck5 = false;
-    private bool finalCorrectCheck=false;
+    private bool finalCorrectCheck = false;
     [SerializeField] private EvidenceData _evidenceData;
     [SerializeField] private GameObject typewriterStuff;
     //This is used to prevent moving between stations when in the typewriter 
@@ -34,7 +34,14 @@ public class TypeWriterController : MonoBehaviour
     private AudioSource _audioSource;
 
     [SerializeField] private AudioClip TypewriterClip;
-    
+    //Keep all the lists in the same order as put below the code will not work properly if all the indexes are not in the correct order
+    //EvidenceID.A01_01, EvidenceID.A01_03, EvidenceID.A01_06, EvidenceID.A01_17, EvidenceID.A04_05,EvidenceID.A02_00, EvidenceID.A01_09, EvidenceID.A04_02, EvidenceID.A03_02, EvidenceID.A03_01, EvidenceID.A05_04
+    [SerializeField] private List<EvidenceID> typewriterID = new List<EvidenceID>();
+    //"Darling King", "Steven Knight", "Guioco Piano", "11:22PM", "The Red Stiletto", "The Alleyway", "A large frame revolver", "a rat","Wanted a personal chest from King","Wanted their inheritance from King","Jealous of Kings life"
+    [SerializeField] private List<string> typewriterText = new List<string>();
+    //1,1,1,3,2,2,4,5,5,5,5
+    [SerializeField] private List<int> typewriterNumber = new List<int>();
+
 
     /// <summary>
     /// Changed this to new input system and moved it's activation to the PlayerController
@@ -174,61 +181,57 @@ public class TypeWriterController : MonoBehaviour
     }
     public void CorrectOption(EvidenceID FirstID,EvidenceData SecondData)
     {
-        //yeahh this code is horrible sorry Zach. I amn going to improve it in a later milestone
+
+        
+      
         EvidenceID SecondID = SecondData.EvidenceID;
-        if ( FirstID== EvidenceID.A01_01 || SecondID== EvidenceID.A01_01)
-        {
-            dropdown1.options.Add(new TMP_Dropdown.OptionData("Darling King", null));
-            dropdown1.RefreshShownValue();
-        }
-        else if (FirstID== EvidenceID.A01_03 || SecondID == EvidenceID.A01_03)
-        {
-            dropdown1.options.Add(new TMP_Dropdown.OptionData("Steven Knight", null));
-            dropdown1.RefreshShownValue();
-        }
-        else if (FirstID == EvidenceID.A01_06 || SecondID == EvidenceID.A01_06)
-        {
-            dropdown1.options.Add(new TMP_Dropdown.OptionData("Guioco Piano", null));
-            dropdown1.RefreshShownValue();
-        }
 
-        if (FirstID == EvidenceID.A01_17 || SecondID == EvidenceID.A01_17)
+        for(int i=0;i<typewriterID.Count;i++)
         {
-            dropdown3.options.Add(new TMP_Dropdown.OptionData("11:22PM", null));
-            dropdown3.RefreshShownValue();
-        }
 
-        if(FirstID==EvidenceID.A04_05||SecondID==EvidenceID.A04_05)
-        {
-            dropdown2.options.Add(new TMP_Dropdown.OptionData("The Red Stiletto", null));
-            dropdown2.RefreshShownValue();
-        }
-        else if (FirstID == EvidenceID.A02_00 || SecondID == EvidenceID.A02_00)
-        {
-            dropdown2.options.Add(new TMP_Dropdown.OptionData("The Alleyway", null));
-            dropdown2.RefreshShownValue();
-        }
-
-        if (FirstID == EvidenceID.A01_09 || SecondID == EvidenceID.A01_09)
-        {
-            dropdown4.options.Add(new TMP_Dropdown.OptionData("A large frame revolver", null));
-            dropdown4.RefreshShownValue();
-        }
-
-        if (FirstID == EvidenceID.A04_02||SecondID==EvidenceID.A04_02)
-        {
-            dropdown5.options.Add(new TMP_Dropdown.OptionData("a rat", null));
-            dropdown5.RefreshShownValue();
-        }
-        else if(FirstID==EvidenceID.A03_02||SecondID==EvidenceID.A03_02 || FirstID == EvidenceID.A03_01 || SecondID == EvidenceID.A03_01)
-        {
-            dropdown5.options.Add(new TMP_Dropdown.OptionData("Wanted their inheritance from King", null));
-            dropdown5.RefreshShownValue();
-        }
-        else if (FirstID == EvidenceID.A04_04 || SecondID == EvidenceID.A04_04 || FirstID == EvidenceID.A05_04 || SecondID == EvidenceID.A05_04)
-        {
-            dropdown5.options.Add(new TMP_Dropdown.OptionData("jealous of Kings life", null));
-            dropdown5.RefreshShownValue();
+            if (FirstID == typewriterID[i] || SecondID == typewriterID[i])
+            {
+                if (typewriterNumber[i]==1)
+                {
+                    dropdown1.options.Add(new TMP_Dropdown.OptionData(typewriterText[i], null));
+                    typewriterID.RemoveAt(i);
+                    typewriterText.RemoveAt(i);
+                    typewriterNumber.RemoveAt(i);
+                    dropdown1.RefreshShownValue();
+                }
+                else if (typewriterNumber[i] == 2)
+                {
+                    dropdown2.options.Add(new TMP_Dropdown.OptionData(typewriterText[i], null));
+                    typewriterID.RemoveAt(i);
+                    typewriterText.RemoveAt(i);
+                    typewriterNumber.RemoveAt(i);
+                    dropdown2.RefreshShownValue();
+                }
+                else if (typewriterNumber[i] == 3)
+                {
+                    dropdown3.options.Add(new TMP_Dropdown.OptionData(typewriterText[i], null));
+                    typewriterID.RemoveAt(i);
+                    typewriterText.RemoveAt(i);
+                    typewriterNumber.RemoveAt(i);
+                    dropdown3.RefreshShownValue();
+                }
+                else if (typewriterNumber[i] == 4)
+                {
+                    dropdown4.options.Add(new TMP_Dropdown.OptionData(typewriterText[i], null));
+                    typewriterID.RemoveAt(i);
+                    typewriterText.RemoveAt(i);
+                    typewriterNumber.RemoveAt(i);
+                    dropdown4.RefreshShownValue();
+                }
+                else
+                {
+                    dropdown5.options.Add(new TMP_Dropdown.OptionData(typewriterText[i], null));
+                    typewriterID.RemoveAt(i);
+                    typewriterText.RemoveAt(i);
+                    typewriterNumber.RemoveAt(i);
+                    dropdown5.RefreshShownValue();
+                }
+            }
         }
     }
     private void Awake()
